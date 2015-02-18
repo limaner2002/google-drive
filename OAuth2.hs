ed76f41bb042325c849cffd73442e08e521bcdb1
Joshua McCartney <josh@iMac.lan>
2015-02-18 12:43:15 -0700

    Cleaned things up a bit.

--- OAuth2.hs
+++ OAuth2.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module OAuth2
     where
 
@@ -6,6 +7,16 @@ import CSRFToken
 import Data.Monoid ((<>))
 import Data.List (intercalate)
 import Network.HTTP.Base (urlEncode)
+import Token
+import Text.Printf
+import System.IO (hFlush, stdout)
+import Network.HTTP.Conduit -- the main module
+import Control.Arrow (second)
+import ConfigFile
+
+import qualified Data.ByteString.Char8 as C8
+import qualified Data.ByteString.Lazy as BL
+
 
 data OAuth2WebServerFlow = OAuth2WebServerFlow
     { token :: !CSRFToken,
@@ -20,8 +31,52 @@ data OAuth2WebServerFlow = OAuth2WebServerFlow
       deviceUri :: !String
     }
 
+createFlow :: String -> String -> IO (OAuth2WebServerFlow)
+createFlow configFile authorizationFile = do
+  conf <- readConfig configFile
+  authorization <- readConfig authorizationFile
+
+  let oauthScope = [param "scopes" conf]
+  let redirectUri = param "redirectUri" conf
+  let userAgent = param "userAgent" conf
+  let authUri = param "authUri" conf
+  let deviceUri = param "deviceUri" conf
+  let revokeUri = param "revokeUri" conf
+  let tokenUri = param "tokenUri" conf
+  let loginHint = param "loginHint" conf
+  let clientId = param "clientId" conf
+  let clientSecret = param "clientSecret" conf
+
+
+  return $ OAuth2WebServerFlow (CSRFToken clientId "someState" "drive")
+         clientSecret oauthScope redirectUri
+         userAgent authUri tokenUri revokeUri loginHint deviceUri
+
 getAuthorizeUrl :: OAuth2WebServerFlow -> String
 getAuthorizeUrl flow = request flow
+
+getTokens :: OAuth2WebServerFlow -> IO (Token)
+getTokens flow = do
+  let tok = token flow
+
+  printf "\nVisit the following URL to retreive a verification code:\n\n"
+  printf "%s\n\n" $ getAuthorizeUrl flow
+  printf "Verification code: "
+  hFlush stdout
+  authCode <- getLine
+
+  let params = [("client_id", clientId tok),
+                ("client_secret", clientSecret flow),
+                ("grant_type", "authorization_code"),
+                ("redirect_uri", redirectUri flow),
+                ("code", authCode)
+               ]
+
+  request <- parseUrl $ tokenUri flow
+
+  result <- withManager $ httpLbs $ urlEncodedBody (map (second C8.pack) params) request
+
+  return $ decode $ responseBody result
 
 instance Show OAuth2WebServerFlow
     where
@@ -30,7 +85,7 @@ instance Show OAuth2WebServerFlow
 instance URI OAuth2WebServerFlow
     where
       render flow = authUri flow
-                    <>"?scope=" <> intercalate "+" (scope flow)
+                    <>"?scope=" <> intercalate "+" (map urlEncode (scope flow))
                     <>"&redirect_uri="<>redirectUri flow
                     <>"&response_type=code"
                     <>"&client_id="<>clientId (token flow)
--- main.hs
+++ main.hs
@@ -1,62 +1,8 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 import OAuth2
-import CSRFToken
-import ConfigFile
 import Text.Printf
-import Token
-
-import qualified Data.ByteString.Char8 as C8
-import qualified Data.ByteString.Lazy as BL
-
-import Network.HTTP.Conduit -- the main module
-import Network.HTTP.Base (urlEncode)
-import System.IO (hFlush, stdout)
-import Control.Arrow (second)
 
 main :: IO ()
 main = do
-  conf <- readConfig "configuration"
-  -- authorization <- readConfig "authorization.txt"
-
-  let oauthScope = map urlEncode [param "scopes" conf]
-  let redirectUri = param "redirectUri" conf
-  let userAgent = param "userAgent" conf
-  let authUri = param "authUri" conf
-  let deviceUri = param "deviceUri" conf
-  let revokeUri = param "revokeUri" conf
-  let tokenUri = param "tokenUri" conf
-  let loginHint = param "loginHint" conf
-  let clientId = param "clientId" conf
-  let clientSecret = param "clientSecret" conf
-
-  let webFlow = OAuth2WebServerFlow (CSRFToken clientId "someState" "drive")
-                clientSecret oauthScope redirectUri
-                userAgent authUri tokenUri revokeUri loginHint deviceUri
-
-
-  printf "\nVisit the following URL to retreive a verification code:\n\n"
-  printf "%s\n\n" $ getAuthorizeUrl webFlow
-  printf "Verification code: "
-  hFlush stdout
-  authCode <- getLine
-
-  let params = [("client_id", clientId),
-                ("client_secret", clientSecret),
-                ("grant_type", "authorization_code"),
-                ("redirect_uri", redirectUri),
-                ("code", authCode)
-               ]
-
-  printf "The authorization code is %s\n" authCode
-
-  req <- parseUrl tokenUri
-  let req2 = urlEncodedBody (map (second C8.pack) params) req
-
-  -- let j = "{  \"access_token\":\"1/fFAGRNJru1FTz70BzhT3Zg\",  \"expires_in\":3920,  \"token_type\":\"Bearer\",  \"refresh_token\":\"1/xEoDL4iW3cxlI7yDbSRFYNG01kVKM2C-259HOF2aQbI\"}"
-  
-  putStrLn $ show req2
-
-  result <- withManager $ httpLbs $ req2
-  -- putStrLn $ show (decode j)
-  putStrLn $ show $ responseBody result
+  webFlow <- createFlow "configuration" "authorization.txt"
+  accessToken <- getTokens webFlow
+  putStrLn $ show accessToken
